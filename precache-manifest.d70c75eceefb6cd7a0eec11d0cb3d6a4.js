60df77e78fa1c4826acb5319dea79c90f8801b3f
nikhilnagargit <nikhilnagarcps@gmail.com>
2020-05-25 20:06:05 +0530

    Updates

---
+++ precache-manifest.d70c75eceefb6cd7a0eec11d0cb3d6a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5ed4c9ab5320786f48d911839ae865",
+    "url": "/React_todo/index.html"
+  },
+  {
+    "revision": "d7b32fead6631dc45b26",
+    "url": "/React_todo/static/css/main.8fa81620.chunk.css"
+  },
+  {
+    "revision": "f18de82ee8ca20120a23",
+    "url": "/React_todo/static/js/2.576b8034.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_todo/static/js/2.576b8034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b32fead6631dc45b26",
+    "url": "/React_todo/static/js/main.031be6fa.chunk.js"
+  },
+  {
+    "revision": "aab62bdee717de85f28c",
+    "url": "/React_todo/static/js/runtime-main.3a03c870.js"
+  }
+]);
